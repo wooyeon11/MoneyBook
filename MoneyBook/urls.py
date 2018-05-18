57ce311bceed533ed3a356148165aaab75ce62dd
Wooyuna4 <wooyuna4@naver.com>
2018-05-18 18:25:53 +0900

    url

--- MoneyBook/urls.py
+++ MoneyBook/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-
-
  urlpatterns = [
-     url(r'^kakao/', include('kakao.urls')),
-     url(r'^admin/', admin.site.urls),]
+ url(r'^kakao/', include('kakao.urls')),
+ url(r'^admin/', admin.site.urls),]
