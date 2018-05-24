c6ab7e3a5b490849b9a43047bcbb0d5cac8ffb3d
Wooyuna4 <wooyuna4@naver.com>
2018-05-24 20:00:53 +0900

    수정

--- kakao/views.py
+++ kakao/views.py
@@ -22,39 +22,7 @@ email="woo4675@gmail.com"
 password="kkcc1313"
 
 user = auth.sign_in_with_email_and_password(email, password)
-data = {
-    "name": "Mortimer 'Morty' Smith",
-    "email": "1234@gmail.com",
-    "operation":"",
-    "when_month":"",
-    "when_day":"",
-    "when_hour":"",
-    "when_start":"",
-    "when_end":"",
-    "who_op":"",
-    "who_with":"",
-    "where":"",
-    "what_op":"",
-    "what_what":""
-}
 
-def insert_data():
-    #db.child("users").push(data)
-    #with key
-    db.child("users").child("Morty").set(data)
-
-
-def update(string_key, string_data):
-    db.child("users").child(string_key).update(string_data)
-
-def remove(string_key):
-    db.child("users").child(string_key).remove()
-
-def get_data(string1):
-    datatmp = db.child("users").child(string1).get()
-    print(datatmp.val())
-
-insert_data()
 
 
 
@@ -74,6 +42,10 @@ def answer(request):
 
     if datacontent == '오늘 쓴 돈':
         today = "10000원입니다"
+        def insert_data():
+            #db.child("users").push(data)
+            #with key
+            db.child("users").child("오늘쓴돈얍").set(data)
 
         return JsonResponse({
                 'message': {
